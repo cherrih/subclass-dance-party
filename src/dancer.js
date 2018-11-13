e818a667578759e081b0e209c84471672cc5c16c
cherrih <cherri.hartigan@gmail.com>
2018-11-12 19:29:36 -0800

    create blinking dancers

--- src/dancer.js
+++ src/dancer.js
@@ -10,22 +10,23 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   // this one sets the position to some random default point within the body
   // dancer.setPosition(top, left);
   this.step();
+  this.setPosition();
 };
 
 makeDancer.prototype.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
     
-    setTimeout(this.step.bind(this), this.timeBetweenSteps);
-  };
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+};
 
-makeDancer.prototype.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    this.$node.css(styleSettings);
+makeDancer.prototype.setPosition = function() {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: this.top,
+    left: this.left
   };
+  this.$node.css(styleSettings);
+};
